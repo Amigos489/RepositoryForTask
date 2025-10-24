48dbb538461c41d2750947dff1ae65dcdedc95e6
Amigos489 <dmitrijkostin874@gmail.com>
2025-10-25 02:14:34 +0300

    add file for task-3

---
+++ Task-3/2/EmployeesFirma.java
@@ -0,0 +1,263 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class EmployeesFirma {
+    public static void main(String[] args) {
+        System.out.println("Фирма N специализируется на производстве программного обеспечения");
+        Employee[] arrayEmployee = new Employee[5];
+        System.out.println("На данный момент в фирме N числится " + arrayEmployee.length + " соотрудников");
+
+        arrayEmployee[0] = new Programmer(22, "Иванов Иван Иванович", 35_000, "Java", "Джун");
+        arrayEmployee[1] = new Programmer(20, "Сергеев Сергей Сергеевич", 35_000, "Java", "Джун");
+        arrayEmployee[2] = new QaEngineer(20, "Николаев Николай Николаевич", 30_000, "Ручное тестирование", "Bugzilla");
+        arrayEmployee[3] = new QaEngineer(26, "Михайлов Михаил Михайлович", 40_000, "Автоматическое тестирование", "Bugzilla");
+        arrayEmployee[4] = new WebDeveloper(28, "Константинов Константин Константинович", 45_000, "JavaScript", "Мидл", "HTML");
+
+        int totalSalary = 0;
+        for (Employee i: arrayEmployee) {
+            totalSalary += i.salary;
+        }
+        System.out.println("Общий ежемесячный оклад составляет: " + totalSalary);
+
+    }
+}
+
+
+abstract class Employee {
+    protected int age; //Возраст
+    protected String FIO; //ФИО
+    protected int salary; //Зарплата
+    protected int workExperience; //Опыт работы
+    protected boolean knowledgeEnglish; //Знание английского языка
+    protected boolean beingOnVacation; //Нахождение в отпуске
+    protected boolean beOnSickLeave; //Нахождение на больничном
+
+
+    //Конструктор по умолчанию
+    Employee() {
+        this.age = 18;
+        this.FIO = "Фамилия Имя Отчество";
+        this.salary = 22_440;
+        this.workExperience = 0;
+        this.knowledgeEnglish = false;
+        this.beingOnVacation = false;
+        this.beOnSickLeave = false;
+    }
+
+    //Конструктор со всеми параметрами
+    Employee(int age, String FIO, int salary, int workExperience, boolean knowledgeEnglish, boolean beingOnVacation, boolean beOnSickLeave) {
+        this.age = age;
+        this.FIO = FIO;
+        this.salary = salary;
+        this.workExperience = workExperience;
+        this.knowledgeEnglish = knowledgeEnglish;
+        this.beingOnVacation = beingOnVacation;
+        this.beOnSickLeave = beOnSickLeave;
+    }
+
+    //Конструктор с 3-мя параметрами
+    Employee(int age, String FIO, int salary) {
+        this.age = age;
+        this.FIO = FIO;
+        this.salary = salary;
+        this.workExperience = 0;
+        this.knowledgeEnglish = false;
+        this.beingOnVacation = false;
+        this.beOnSickLeave = false;
+    }
+
+    //геттеры и сеттеры
+
+    int getAge() {
+        return this.age;
+    }
+
+    void setAge(int newValue) {
+        if (newValue >= 18 && newValue >= 65) {
+            this.age = newValue;
+            return;
+        }
+        System.out.println("Введено некорректное значение!");
+    }
+
+    String getFIO() {
+        return this.FIO;
+    }
+
+    int getSalary() {
+        return this.salary;
+    }
+
+    void setSalary(int newValue) {
+        if (newValue < 22_440) {
+            this.salary = newValue;
+            return;
+        }
+        System.out.println("Введено некорректное значение!");
+    }
+
+    int getWorkExperience() {
+        return this.workExperience;
+    }
+
+    void setWorkExperience(int newValue) {
+        if (newValue < 0) {
+            this.workExperience = newValue;
+            return;
+        }
+        System.out.println("Введено некорректное значение!");
+    }
+
+    boolean getKnowledgeEnglish() {
+        return this.knowledgeEnglish;
+    }
+
+    void setKnowledgeEnglish(boolean newValue) {
+        this.knowledgeEnglish = newValue;
+    }
+
+    //Уйти в отпуск
+    void goOnVacation() {
+        this.beingOnVacation = true;
+    }
+
+    //Вернуться из отпуска
+    void ReturnFromVacation() {
+        this.beingOnVacation = false;
+    }
+
+    //Уйти на больничный
+    void goOnSickLeave() {
+        this.beOnSickLeave = true;
+    }
+
+    //Вернуться с больничного
+    void ReturnFromSickLeave() {
+        this.beOnSickLeave = false;
+    }
+
+}
+
+class Programmer extends Employee{
+    protected String programmingLanguage;
+    protected String classificationLevel;
+
+
+    //Конструктор по умолчанию
+    Programmer() {
+        super();
+        this.programmingLanguage = "Язык программирования";
+        this.classificationLevel = "Уровень квалификации";
+    }
+
+    //Конструктор со всеми параметрами
+    Programmer(int age, String FIO, int salary, int workExperience, boolean knowledgeEnglish, boolean beingOnVacation, boolean beOnSickLeave, String programmingLanguage, String classificationLevel) {
+        super(age, FIO, salary, workExperience, knowledgeEnglish, beingOnVacation, beOnSickLeave);
+        this.classificationLevel = classificationLevel;
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    //Конструктор со 5-ю параметрами
+    Programmer(int age, String FIO, int salary, String programmingLanguage, String classificationLevel) {
+        super(age, FIO, salary);
+        this.classificationLevel = classificationLevel;
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    //сеттеры и геттеры
+    String getProgrammingLanguage() {
+        return this.programmingLanguage;
+    }
+
+    void setProgrammingLanguage(String newValue) {
+        this.programmingLanguage = newValue;
+    }
+
+    String getClassificationLevel() {
+        return this.classificationLevel;
+    }
+
+    void setClassificationLevel(String newValue) {
+        this.classificationLevel = newValue;
+    }
+}
+
+class WebDeveloper extends Programmer {
+    protected String hypertextMarkupLanguage;
+
+    //Конструктор по умолчанию
+    WebDeveloper() {
+        super();
+        this.hypertextMarkupLanguage = "Язык гипертекстовой разметки";
+    }
+
+    //Конструктор со всеми параметрами
+    WebDeveloper(int age, String FIO, int salary, int workExperience, boolean knowledgeEnglish, boolean beingOnVacation, boolean beOnSickLeave, String programmingLanguage, String classificationLevel, String hypertextMarkupLanguage) {
+        super(age, FIO, salary, workExperience, knowledgeEnglish, beingOnVacation, beOnSickLeave, programmingLanguage, classificationLevel);
+        this.hypertextMarkupLanguage = hypertextMarkupLanguage;
+    }
+
+    //Конструктор с 6-ю параметрами
+    WebDeveloper(int age, String FIO, int salary, String programmingLanguage, String classificationLevel, String hypertextMarkupLanguage) {
+        super(age, FIO, salary, programmingLanguage, classificationLevel);
+        this.hypertextMarkupLanguage = hypertextMarkupLanguage;
+    }
+
+    //Сеттры и геттеры
+
+    String getHypertextMarkupLanguage() {
+        return this.hypertextMarkupLanguage;
+    }
+
+    void setHypertextMarkupLanguage(String newValue) {
+        this.hypertextMarkupLanguage = newValue;
+    }
+}
+
+class QaEngineer extends Employee {
+    String typeTesting;
+    String bugTrackingSystem;
+
+    //Конструктор по умолчанию
+    QaEngineer() {
+        super();
+        this.typeTesting = "Вид тестирования";
+        this.bugTrackingSystem = "Система отслеживания ошибок";
+    }
+
+    //Конструктор с 5-ю параметрами
+    QaEngineer(int age, String FIO, int salary, String typeTesting, String bugTrackingSystem) {
+        super(age, FIO, salary);
+        this.typeTesting = typeTesting;
+        this.bugTrackingSystem = bugTrackingSystem;
+    }
+
+    //Конструктор со всеми параметрами
+    QaEngineer(int age, String FIO, int salary, int workExperience, boolean knowledgeEnglish, boolean beingOnVacation, boolean beOnSickLeave, String typeTesting, String bugTrackingSystem) {
+        super(age, FIO, salary, workExperience, knowledgeEnglish, beingOnVacation, beOnSickLeave);
+        this.typeTesting = typeTesting;
+        this.bugTrackingSystem = bugTrackingSystem;
+    }
+
+    //геттеры и сеттеры
+    String getTypeTesting() {
+        return this.typeTesting;
+    }
+
+    void setTypeTesting(String typeTesting) {
+        this.typeTesting = typeTesting;
+    }
+
+    String getBugTrackingSystem() {
+        return this.bugTrackingSystem;
+    }
+
+    void setBugTrackingSystem(String typeTesting) {
+        this.bugTrackingSystem = bugTrackingSystem;
+    }
+
+
+
+
+
+}
---
+++ Task-3/4/BookStore.java
@@ -0,0 +1,324 @@
+public class BookStore {
+    public static void main(String[] args) {
+        System.out.println("Электронный магазин книг.");
+
+        //Создание книг
+        Book b1984 = new Book("Книга", true, 20, "1984", "Д.Оруэлл", 340);
+        Book bMasterAndMargaret = new Book("Книга", true, 50, "Мастер и Маргарита", "М.Булгаков", 480);
+        Book bAllQuiet = new Book("Книга", true, 40, "На западном фронте без перемен", "Э.М.Ремарк", 360);
+
+
+        //Создание склада
+        Warehouse wh = new Warehouse(0, 100);
+
+        //Помещаем книги на склад
+        wh.addWarehouse(b1984);
+        wh.addWarehouse(bMasterAndMargaret);
+        wh.addWarehouse(bAllQuiet);
+
+        //Списываем со склада
+        wh.writeFromWarehouse(bMasterAndMargaret);
+
+        //Повторная попытка добавить на склад
+        wh.addWarehouse(bAllQuiet);
+
+        //Создаём заказ
+        Order orderAllQuiet = new Order(bAllQuiet);
+        orderAllQuiet.createOrder();
+
+        //Создаём заказ на книгу, которой нет в наличии
+        Order orderMasterAndMargaret = new Order(bMasterAndMargaret);
+        orderMasterAndMargaret.createOrder();
+
+
+    }
+}
+
+//Абстрактный класс продукта
+abstract class Product {
+    protected String nameProduct;
+    protected boolean Availability;
+    protected int numperProduct;
+
+    //Конструктор по умолчанию
+    public Product() {
+        this.nameProduct = "Продукт";
+        this.Availability = false;
+        this.numperProduct = 0;
+    }
+
+    //Конструтор со всеми параметрами
+    public Product(String nameProduct, boolean Availability, int numperProduct) {
+        this.nameProduct = nameProduct;
+        this.Availability = Availability;
+        this.numperProduct = numperProduct;
+    }
+    
+    //Геттеры и сеттеры
+    public String getNameProduct() {
+        return this.nameProduct;
+    }
+
+    public boolean getAvailability() {
+        return this.Availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        Availability = availability;
+    }
+
+    public int getNumberProduct() {
+        return this.numperProduct;
+    }
+
+    public void setNumberProduct(int newValue) {
+        if (newValue >= 0) {
+            this.numperProduct = newValue;
+        }
+        else {
+            System.out.println("Ошибка! некорректное значение");
+        }
+    }
+}
+
+//Книга
+class Book extends Product{
+    private String nameBook;
+    private String authorBook;
+    private int numberPages;
+
+    // Конструктор по умолчанию
+    public Book() {
+        super();
+        this.nameBook = "Неизвестно";
+        this.authorBook = "Неизвестен";
+        this.numberPages = 0;
+    }
+
+    // Конструктор со всеми параметрами
+    public Book(String nameProduct, boolean availability, int numperProduct, String nameBook, String authorBook, int numberPages) {
+        super(nameProduct, availability, numperProduct);
+        this.nameBook = nameBook;
+        this.authorBook = authorBook;
+        this.numberPages = numberPages;
+    }
+
+    //Геттеры и сеттеры
+    public String getNameBook() {
+        return this.nameBook;
+    }
+
+    public String getAuthorBook() {
+        return this.authorBook;
+    }
+
+    public int getNumberPages() {
+        return this.numberPages;
+    }
+
+}
+
+
+
+//Интерфейс для работы со складом
+public interface IWorkingWithWarehouse {
+    public void addWarehouse(Product product);
+    public void writeFromWarehouse(Product product);
+}
+
+//Склад
+class Warehouse implements IWorkingWithWarehouse {
+    private int numberAllProduct;
+    private int maxNumberProduct;
+
+    Warehouse() {
+        this.numberAllProduct = 0;
+        this.maxNumberProduct = 100;
+    }                      
+
+    Warehouse(int numberAllProduct, int maxNumberProduct) {
+        if (numberAllProduct >= 0 && maxNumberProduct > 0) {
+            this.numberAllProduct = numberAllProduct;
+            this.maxNumberProduct = maxNumberProduct;
+        }
+        else {
+            System.out.println("Ошибка! введено некорректное значение");
+        }
+    }
+
+    //Добавить предмет на склад
+    public void addWarehouse(Product product) {
+        this.setNumberAllProduct(product.getNumberProduct());
+    }
+
+    //Списать предмет со склада
+    public void writeFromWarehouse(Product product) {
+        this.numberAllProduct -= product.getNumberProduct();
+        product.setNumberProduct(0);
+        product.setAvailability(false);
+        System.out.println("Продукт списан со склада");
+    }
+
+
+    //Геттеры и сеттеры
+    public int getNumberAllProduct() {
+        return this.numberAllProduct;
+    }
+
+    public void setNumberAllProduct(int newValue) {
+        if (this.numberAllProduct + newValue <= this.maxNumberProduct) {
+            this.numberAllProduct += newValue;
+            System.out.println("Продукт добавлен на склад");
+        }
+        else {
+            System.out.println("Ошибка! на складе недостаточно места");
+        }
+    }
+
+    public int getMaxNumberBook() {
+        return this.maxNumberBook;
+    }
+}
+
+//Интерфейс для работы с заказами 
+public interface IWorkingWithOrder {
+    public void createOrder();
+    public void cancelOrder();
+    public void changeOrder(String newValue);
+}
+
+//Заказ на книгу
+class Order implements IWorkingWithOrder{
+    private int OrderID;
+    private String statusOrder;
+    private Product product;
+
+    //Конструктор по умолчанию (отношение композиция, так как Заказ не может существовать без продукта)
+    Order(Product product) {
+        this.OrderID = 0;
+        this.statusOrder = "отменён";
+        this.product = product;
+    }
+
+    //Создание заказа
+    public void createOrder() {
+        if (this.product.Availability) {
+            this.statusOrder = "новый";
+            System.out.println("Создаётся заказ на продукт " + product.nameProduct);
+            product.setNumberProduct(product.getNumberProduct() - 1);
+            System.out.println("Теперь на складе продуктов: " + product.nameProduct  + " осталось " + product.numperProduct);
+            System.out.println("Заказ отправлен и получен.");
+            this.statusOrder = "выполнен";
+        }
+        else if (!this.product.Availability) {
+            this.statusOrder = "отменен";
+            System.out.println("Продукта нет в наличии");
+            IWorkingWithRequest request = new Request(this);
+            request.createRequest();
+
+            //Условность, спустя время книга появляется в наличии. В дальнейшем будет исправлено
+            product.setNumberProduct(5);
+            product.setAvailability(true);
+            
+            request.cancelRequest();
+
+        }
+    }
+
+    //Отмена заказа
+    public void cancelOrder() {
+        if (this.statusOrder == "новый") {
+            this.statusOrder = "отменен";
+            product.setNumberProduct(this.statusBookOrder = "отменен");
+            System.out.println("Заказ на продукт отменён");
+            System.out.println("Теперь на складе книг: " + this.book.getNumberCopies());
+            
+        }
+        else {
+            System.out.println("Данный заказ нельзя отменить, потому что он уже выполнен или отменен");
+        }
+    }
+
+    //Изменение статуса заказа
+    public void changeOrder(String newValue) {
+        this.setStatusOrder(newValue);
+    }
+
+    //Геттеры и сеттеры
+    public int getOrderID() {
+        return OrderID;
+    }
+
+    public String getStatusOrder() {
+        return statusOrder;
+    }
+
+    public void setStatusOrder(String newValue) {
+        if (newValue == "новый" || newValue == "отменен" || newValue == "выполнен") {
+            this.statusOrder = newValue;
+        }
+        else {
+            System.out.println("Ошибка! задано некорректное значение");
+        }
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+}
+
+//Интерфейс для работы с запросами 
+public interface IWorkingWithRequest {
+    public void createRequest();
+    public void cancelRequest();
+}
+
+//Запрос на книгу
+class Request implements IWorkingWithRequest {
+    private int RequestsID;
+    private String statusRequest;
+    private IWorkingWithOrder order;
+    
+    //Конструктор по умолчанию
+    Request(IWorkingWithOrder order) {
+        this.RequestsID = new java.util.Random().nextInt(100);
+        this.statusRequest = "новый";
+    }
+
+    //Создание запроса
+    public void createRequest() {
+            System.out.println("Создан запрос на продукт.");
+            System.out.println("Ожидайте его появления");
+    }
+
+    //завершение запроса
+    public void cancelRequest() {
+        this.statusRequest = "завершен";
+        System.out.println("Продукт появился в наличии");
+        System.out.println("Запрос на продукт завершен.");
+    }
+
+    //Геттеры и сеттеры
+    public int getRequestsID() {
+        return RequestsID;
+    }
+
+    public String getStatusRequest() {
+        return statusRequest;
+    }
+
+    public void setStatusRequest(String newValue) {
+        if (newValue == "новый" || newValue == "завершен") {
+            this.statusRequest = newValue;
+        }
+        else {
+            System.out.println("Ошибка! задано некорректное значение");
+        }
+    }
+
+    public IWorkingWithOrder getOrder() {
+        return order;
+    }
+
+}
