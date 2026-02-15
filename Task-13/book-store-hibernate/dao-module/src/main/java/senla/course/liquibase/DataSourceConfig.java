c0cddb15bfdbdfd53fbc2dacbc61c568656c18c4
Balabol <dmitrijkostin874@gmail.com>
2026-02-15 23:16:13 +0300

    add integration liquibase (Task-15)

---
+++ Task-13/book-store-hibernate/dao-module/src/main/java/senla/course/liquibase/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package senla.course.liquibase;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("org.postgresql.Driver");
+        ds.setUrl("jdbc:postgresql://localhost:5432/book-store-hibernate");
+        ds.setUsername("postgres");
+        ds.setPassword("balabol");
+        return ds;
+    }
+}
---
+++ Task-13/book-store-hibernate/dao-module/src/main/resources/db/changelog/V6_insert_request.sql
@@ -0,0 +1,4 @@
+--changeset balabol:6
+INSERT INTO request (bookId,countRequest,isClosed)
+VALUES(4,3,true),
+      (5,1,true);
